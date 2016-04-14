b6b214e3a00f251f84805c56a12c70616a2b13ec
Dhivya Janakiraman <dhivya.janakiraman91@gmail.com>
2016-04-13 23:52:01 -0700

    Updated server.js with POST request to backend

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
-// server.js
+var express        =         require("express");
+var bodyParser     =         require("body-parser");
+var app            =         express();
 
-// set up ======================================================================
-// get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-//var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
-//var configDB = require('./config/database.js');
-
-// configuration ===============================================================
-//mongoose.connect(configDB.url); // connect to our database
-
-require('./config/passport')(passport); // pass passport for configuration
-
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-
-app.set('view engine', 'ejs'); // set up ejs for templating
-
-// required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-// routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+app.get('/',function(req,res){
+  res.sendfile("index.html");
+});
+app.post('/login',function(req,res){
+  var user_name=req.body.user;
+  var password=req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
+});
+app.listen(8080,function(){
+  console.log("Started on PORT 8080");
+})
