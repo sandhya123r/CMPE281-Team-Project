4728ed8dad9fc955ce59e1fc6fc3079a861858d9
Sindhoori Rupaakula <rsindhoori@gmail.com>
2016-04-15 13:32:19 -0700

    starter code for understanding nodejs

---
+++ nodejs_starter/nodejs-app/server.js
@@ -0,0 +1,11 @@
+
+//Add express to the app
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){	
+	res.send("Hello World");
+});
+
+app.listen(3000);
+module.exports = app;
