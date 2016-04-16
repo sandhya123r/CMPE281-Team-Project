3e03405fe91c5cadb170f7d23cf51e7acee19a90
Dhivya Janakiraman <dhivya.janakiraman91@gmail.com>
2016-04-15 22:37:09 -0700

    Adding passport authentication framework for login and signup

--- backup/server.js
+++ backup/server.js
@@ -13,6 +13,7 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var fs = require('fs');
 
 //var configDB = require('./config/database.js');
 
--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@
 var LocalStrategy   = require('passport-local').Strategy;
 
 // load up the user model
-//var User            = require('../app/models/user');
+var User            = require('../app/models/user');
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
@@ -22,11 +22,9 @@ module.exports = function(passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        /*
         User.findById(id, function(err, user) {
             done(err, user);
         });
-        */
     });
 
     // =========================================================================
@@ -45,13 +43,10 @@ module.exports = function(passport) {
 
         // asynchronous
         // User.findOne wont fire unless data is sent back
-        process.stdout.write("hello1: ");
         process.nextTick(function() {
-            process.stdout.write("hello2: ");
-            return done(null);
+
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        /*
         User.findOne({ 'local.email' :  email }, function(err, user) {
             // if there are any errors, return the error
             if (err)
@@ -78,9 +73,38 @@ module.exports = function(passport) {
                 });
             }
 
-        }); 
-        */   
+        });    
 
+        });
+
+    }));
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    
+    function(req, email, password, done) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+            // if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
         });
 
     }));
