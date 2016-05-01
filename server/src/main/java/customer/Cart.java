740a1d6b650d4b38c7b4d59a16cba77e7fb64b83
Sandhya R <sandhyafeb1990@gmail.com>
2016-05-01 09:33:48 +0000

    backend changes

--- server/src/main/java/customer/Cart.java
+++ server/src/main/java/customer/Cart.java
@@ -6,11 +6,13 @@ public class Cart {
 
     private String cartId;
     private Map <String, Integer> items;
-	
+
+    private List<Map<String, String>> cart_to_display;
 
     public Cart() {
         this.setCartId();
         this.setItems();
+        this.setCart_to_display();
     }
 
     public Map<String, Integer> getItems() {
@@ -20,9 +22,20 @@ public class Cart {
     public String getCartId() {
         return cartId;
     }
-    
+
+    public List<Map<String, String>> getCart_to_display() {
+        return cart_to_display;
+    }
+
     public void setItems(){
         this.items = new HashMap <String, Integer>();	
+    }
+    public void setCart_to_display(){
+        this.cart_to_display = new ArrayList <Map<String, String>>();	
+    }
+
+    public void setCart_to_display(List<Map<String, String>> newCart_to_display){
+        this.cart_to_display = newCart_to_display;
     }
 
     public void setItems(Map<String,Integer> items) {
